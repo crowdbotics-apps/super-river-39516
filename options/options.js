b3cbf968c74774e0f1785c258704d1059cc42efc
Crowdbotics <team@crowdbotics.com>
2023-03-23 02:15:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-river-39516",
+  url: "https://super-river-39516.botics.co",
+  api: "https://super-river-39516.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
